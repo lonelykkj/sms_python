b5bf842dee4a5d25dca2255c36d5f77fd374c8e5
Heitor Carvalho <128411446+lonelykkj@users.noreply.github.com>
2025-03-20 10:30:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-#2LZ9BMEXFNU7G73U6NGH78NZ RECOVERY CODE
-
 from fastapi import FastAPI, HTTPException
 from twilio.rest import Client
 
 app = FastAPI()
 
-ACCOUNT_SID = 'AC6e1ad92c274dd4b4e3475b5480a1ba88'
-AUTH_TOKEN = '9c92e9d5352e4f2a46e2a0f06474a102'
-PHONE_NUMBER = '+16505822353'
+ACCOUNT_SID = 'twiolio account sid'
+AUTH_TOKEN = 'twilio auth token'
+PHONE_NUMBER = 'twilio number'
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
@@ -16,7 +14,7 @@ async def send_sms(to: str, body: str):
     try:
         message = client.messages.create(
             from_=PHONE_NUMBER,
-            to='+5512982998077',
+            to='your number',
             body='Hello World!'
         )
         return {'status': 'success', 'message_sid': message.sid}
